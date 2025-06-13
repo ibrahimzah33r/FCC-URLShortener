b46eb6de7b647fe0fdd2771ef8acee51f7f26237
GilTorch <infotorch2014@gmail.com>
2025-06-13 12:43:57 -0400

    add endpoint to create url shorterner and redirect

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
-
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 // Basic Configuration
 const port = process.env.PORT || 3000;
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded())
+
+// parse application/json
+app.use(bodyParser.json())
 
 app.use(cors());
 
@@ -18,6 +25,77 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+
+
+const MONGO_URI = process.env.MONGO_URI;
+
+const connection = mongoose.createConnection(MONGO_URI);
+
+
+const shortUrlSchema = mongoose.Schema({
+  url: {
+    type: String, 
+    required: true
+  }, 
+  prefix: {
+    type: Number, 
+    required: true
+  }
+})
+
+const ShortUrl = connection.model("ShortUrl", shortUrlSchema);
+
+
+
+app.post('/api/shorturl', async (req, res) => {
+
+  const url = req.body.url; 
+
+  ShortUrl.findOne({ url }, async (err,data) => {
+    if(err) return res.status(500).json({ message: "Something unexpected happened"});
+    if(!data){
+          
+      let shortUrlCount = 0;
+          
+          try {
+            const count = await ShortUrl.countDocuments();
+            shortUrlCount = count;
+
+          } catch(err){
+            return res.status(500).json({ message: err })
+          }
+
+          const data = {
+            url,
+            prefix: shortUrlCount + 1
+          }
+
+          const shortUrl = new ShortUrl(data)
+
+          shortUrl.save(data, (err, data) => {
+            if(err){
+              return res.status(500).json({ message: "Something unexpected happened"})
+            }else {
+              return res.status(200).json({ url: data.url, shortId: data.prefix })
+            }
+          })
+    } else {
+      return res.status(200).json({url: data.url, shortId: data.prefix});
+    }
+  })
+})
+
+app.get("/api/shortUrl/:shortId", async (req, res) => {
+  const shortId = req.params.shortId; 
+
+  ShortUrl.findOne({ prefix: shortId }, (err, data) => {
+    if(err) return res.json(500).status({ message: "Something unexpected happened" })
+    res.redirect(data.url);
+  })
+
+})
+
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
